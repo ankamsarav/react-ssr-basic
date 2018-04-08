4394e7301e17cf3dce3d774c0d03f4dfbc1c33b1
Saravana <ankamsarav@gmail.com>
2018-04-08 18:55:31 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# React Server-side Rendering
+
+Install dependencies:
+
+```
+npm install
+```
+
+Start the server:
+
+```
+npm start
+```
+
+For checking page with out SSR rename the hello.js to someother name and change the hellowithoutssr.js to hello.js
+Similarly rename myserver.jsss to server.js and change the existing server.js to someother name.
---
+++ hellowithoutssr.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Butter from 'buttercms'
+
+const butter = Butter('b60a008584313ed21803780bc9208557b3b49fbb');
+
+class Hello extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false
+    }
+  }
+
+  componentWillMount(){
+    butter.post.list().then((resp) => {
+      this.setState({
+        loaded: true,
+        resp: resp.data
+      })
+    });
+  }
+
+  render(){
+    if (this.state.loaded) {
+      return (
+        <div>
+          {this.state.resp.data.map((post) => {
+            return (
+              <div key={post.slug}>{post.title}</div>
+            )
+          })}
+        </div>
+      );
+    } else {
+      return <div>Loading...</div>;
+    }
+  }
+}
+
+export default Hello;
